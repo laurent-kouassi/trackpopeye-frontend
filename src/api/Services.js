a6390bbefbc76078c84833f590d10ace51b802dd
Laurent Kouassi <klaurent911@gmail.com>
2022-01-18 00:14:11 +0300

    fixed

--- src/api/Services.js
+++ src/api/Services.js
@@ -4,11 +4,11 @@ import { BASE_URL } from "./Env";
 
 export const getRequest = async (routeId) => {
            await axios.get(BASE_URL + routeId)
-                      .then(result => {
-                        return JSON.stringify(result);
-                       })
-                      .catch(error => {
-                         console.log(error);
-                         return error;
-                      })
+                     //  .then(result => {
+                     //    return JSON.stringify(result);
+                     //   })
+                     //  .catch(error => {
+                     //     console.log(error);
+                     //     return error;
+                     //  })
    };
